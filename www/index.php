3a5d0bb888b58e4ccade48c1c962f06cc7f6d5a0
Christian Rey <director@elr3y.com>
2025-07-04 16:22:39 -0700

    favicon

--- www/index.php
+++ www/index.php
@@ -27,6 +27,10 @@
   <meta name="twitter:description" content="Revive los mejores momentos del gaming retro: música, videos y manuales originales." />
   <meta name="twitter:image" content="media.png" />
 
+  <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
+  <link rel="manifest" href="favicon/site.webmanifest">
 
   <!-- Bootstrap CSS para estilos responsive y modernos -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
